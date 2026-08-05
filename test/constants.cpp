a9689d2dbdfd021f2482a50642f3be6db776a706
Ivan <ivanbabaev217@gmail.com>
2026-08-06 00:27:57 +0800

    Make float constant tests actually floats

--- test/constants.cpp
+++ test/constants.cpp
@@ -41,36 +41,36 @@ TEST(Constants, vec2i_right)
 
 TEST(Constants, vec2f_zero)
 {
-    EXPECT_EQ((VEC2F_ZERO).x, 0);
-    EXPECT_EQ((VEC2F_ZERO).y, 0);
+    EXPECT_EQ((VEC2F_ZERO).x, 0.0f);
+    EXPECT_EQ((VEC2F_ZERO).y, 0.0f);
 }
 
 TEST(Constants, vec2f_one)
 {
-    EXPECT_EQ((VEC2F_ONE).x, 1);
-    EXPECT_EQ((VEC2F_ONE).y, 1);
+    EXPECT_EQ((VEC2F_ONE).x, 1.0f);
+    EXPECT_EQ((VEC2F_ONE).y, 1.0f);
 }
 
 TEST(Constants, vec2f_up)
 {
-    EXPECT_EQ((VEC2F_UP).x,  0);
-    EXPECT_EQ((VEC2F_UP).y, -1);
+    EXPECT_EQ((VEC2F_UP).x,  0.0f);
+    EXPECT_EQ((VEC2F_UP).y, -1.0f);
 }
 
 TEST(Constants, vec2f_left)
 {
-    EXPECT_EQ((VEC2F_LEFT).x, -1);
-    EXPECT_EQ((VEC2F_LEFT).y,  0);
+    EXPECT_EQ((VEC2F_LEFT).x, -1.0f);
+    EXPECT_EQ((VEC2F_LEFT).y,  0.0f);
 }
 
 TEST(Constants, vec2f_down)
 {
-    EXPECT_EQ((VEC2F_DOWN).x, 0);
-    EXPECT_EQ((VEC2F_DOWN).y, 1);
+    EXPECT_EQ((VEC2F_DOWN).x, 0.0f);
+    EXPECT_EQ((VEC2F_DOWN).y, 1.0f);
 }
 
 TEST(Constants, vec2f_right)
 {
-    EXPECT_EQ((VEC2F_RIGHT).x, 1);
-    EXPECT_EQ((VEC2F_RIGHT).y, 0);
+    EXPECT_EQ((VEC2F_RIGHT).x, 1.0f);
+    EXPECT_EQ((VEC2F_RIGHT).y, 0.0f);
 }
